530800404eb6121f37dbe01a453374402244470c
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 02:55:09 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,8 +3,11 @@ def reformat_languages(input)
   input.each do |style, languages|
     languages.each do |language, attributes|
       reformatted[language] = attributes
-      
+      if reformatted[language][:style]
+        reformatted[language][:style] << style
+      else
         reformatted[language][:style] = [style]
+      end
     end
   end
   reformatted
